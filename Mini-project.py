3200d5e0ee55bb418591318171e3ccf992672263
NickBoe <NickBoe@users.noreply.github.com>
2019-02-20 00:59:09 +0100

    ss

--- Mini-project.py
+++ Mini-project.py
@@ -1,50 +1,66 @@
 import cv2
-import numpy
+import math
+import numpy as np
 
-src = cv2.imread("lena.png", 0)
+class Rotate:
+    #Image import
+    src = cv2.imread('lena.png', 0)
+    rows, cols = src.shape
 
-#Forward mapping method
-def forMap (img, Bx, By):
-    rows, cols = img.shape
-    #Canvas ensuring correct window size
-    imgForward = numpy.ndarray(shape = (int (rows + cols*abs(By)), int (cols + rows*abs(Bx))))
+    #Rotation angle in radians
+    angle = 22.5
+    rad = angle * float(math.pi / 180)
 
-    #For loops to go through all rows and collums
-    for row in range(rows):
-        for col in range(cols):
-            #Apply the forward mapping algorithm
-            fCol = int (col+row*Bx)
-            fRow = int (row+col*By)
-            imgForward[fRow, fCol] = img[row,col]/255
+    # Rotation point
+    x = 50
+    y = 40
 
-    return imgForward
+    # Forward mapping function
+    def forMap(self, x, y, rad):
 
-#Backward mapping method
-def backMap (img, Bx, By):
-    rows, cols = img.shape
-    #Canvas ensuring correct window size in accordance to the img.shape parameters
-    imgBackward = numpy.ndarray(shape = img.shape)
-    n = int(1/(1-Bx*By))
+        rows, cols = self.shape
+        imgForward = np.zeros(shape=(int(rows), int(cols)), dtype=np.uint8)
 
-    #For loops to go through all rows and collums
-    for row in range(rows):
-        for col in range(cols):
-            #Apply the backward mapping algorithm
-            bCol = int(n*(col+row*-Bx))
-            bRow = int(n*(row+col*-By))
-            imgBackward[bRow, bCol] = img[row, col]
+        for row in range(rows):
+            for col in range(cols):
+                fCol = int(((col - x) * math.cos(rad)) - ((row - y) * math.sin(rad)) + x)
+                fRow = int(((col - x) * math.sin(rad)) + ((row - y) * math.cos(rad)) + y)
 
-    return imgBackward
+                if fCol < cols and fRow < rows and fCol > 0 and fRow > 0:
+                    imgForward[fRow, fCol] = self[row, col]
+                else:
+                    pass
 
-#Shearing factors
-Bx = 0.3
-By = 0.2
+        return imgForward
 
-ForMapping = forMap(src, Bx, By)
-BackMapping = backMap(ForMapping, Bx, By)
+    ForMapping = forMap(src, x, y, rad)
+    cv2.imshow("Forward mapped", ForMapping)
 
-cv2.imshow("Original", src)
-cv2.imshow("Forward mapped", ForMapping)
-cv2.imshow("Backward mapped", BackMapping)
+    # Backward mapping function
+    def backMap(self, img):
 
-cv2.waitKey(0)
+        imgBackward = np.zeros((self.rows, self.cols), dtype=np.uint8)
+
+        for row in range(self.rows):
+            for col in range(self.cols):
+                bCol = int(((col - self.x) * math.cos(self.rad) + (row - self.y) * math.sin(self.rad))+self.x)
+                bRow = int((-(col - self.x) * math.sin(self.rad) + (row - self.y) * math.cos(self.rad))+self.y)
+
+                if bCol < self.cols and bRow < self.rows and bCol > 0 and bRow > 0:
+                    imgBackward[row, col] = self.src[bRow, bCol]
+                else:
+                    pass
+
+        return imgBackward
+
+    def showImage(name, self):
+        cv2.imshow(name, self.src)
+
+def output():
+    Rotate.showImage('Original', Rotate)
+    cv2.imshow('Backward Mapping', Rotate.backMap(Rotate, Rotate))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+
+if __name__ == '__main__':
+    output()
